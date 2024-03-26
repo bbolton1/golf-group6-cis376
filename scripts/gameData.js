1751410550acd738b6d00faa828a228f00ffeff1
Brayden Bolton <157667096+bbolton1@users.noreply.github.com>
2024-03-26 13:51:53 -0500

    Add files via upload

---
+++ scripts/gameData.js
@@ -0,0 +1,161 @@
+const gameData = [
+  
+    {
+        gameURL: "https://marvin-2056.github.io/Delta-CIS-367/",
+        gameName: "Find your spirit animal", 
+        gameAuthor: "Marvin De La Cruz", 
+        authorGithubName: "marvin-2506",
+    },
+    {
+        gameURL: "https://bbolton1.github.io/delta-lab-brayden/",
+        gameName: "What's your DnD race?", 
+        gameAuthor: "Brayden Bolton",
+        authorGithubName: "bbolton1"
+    },
+    {
+        gameURL: "https://zrundle.github.io/charlie-lab-zrundle/",
+        gameName: "An Interactive Character Gallery", 
+        gameAuthor: "Zachary Rundle", 
+        authorGithubName: "zrundle",
+    },
+    {
+        gameURL: "https://github.com/devan4264/improved-octo-chainsaw-charlie",
+        gameName: "Feed the doge", 
+        gameAuthor: "Devan Bush", 
+        authorGithubName: "devan4264",
+    },
+    {
+        gameURL: "https://mackenzie24t.github.io/legendary-goggles-charlie/",
+        gameName: "Drag doge to the right to win!", 
+        gameAuthor: "Mackenzie Thompson", 
+        authorGithubName: "mackenzie24t",
+    },
+    {
+        gameURL: "https://johnallenb.github.io/crispy-charlie-lab03-blaxton/",
+        gameName: "Play with some papyrus and comic sans gifs!", 
+        gameAuthor: "John Allen Blaxton", 
+        authorGithubName: "johnallenb",
+    },
+    {
+        gameURL: "https://babayaga1013.github.io/charlie-lab-cis376/",
+        gameName: "Who's the goat of tech?", 
+        gameAuthor: "...", 
+        authorGithubName: "babayaga1013",
+    },
+    {
+        gameURL: "https://jacob-vance.github.io/turbo-pancake-charlie/",
+        gameName: "Help doge across the screen", 
+        gameAuthor: "Jacob Vance", 
+        authorGithubName: "jacob-vance",
+    },
+    {
+        gameURL: "https://jaydengreen23.github.io/delta-jubilant-octo-winner/",
+        gameName: "Exchange your name for a new one!", 
+        gameAuthor: "Jayden Green", 
+        authorGithubName: "jaydengreen23",
+    },
+    {
+        gameURL: "https://smaldonado1326.github.io/cuddly-invention--charlie/",
+        gameName: "Play hot or cold with doge", 
+        gameAuthor: "Sam Maldonada", 
+        authorGithubName: "smaldonado1326",
+    },
+    {
+        gameURL: "https://nrobinson4.github.io/bug-free-potato-charlie/",
+        gameName: "Help doge fall asleep!", 
+        gameAuthor: "Nolen Robinson", 
+        authorGithubName: "nrobinson4",
+    },
+    {
+        gameURL: "https://michaelhaydenduran.github.io/silly-dino-charlie/",
+        gameName: "It sure would be shame if doge fell into a whole...", 
+        gameAuthor: "Michael Hayden Duran", 
+        authorGithubName: "michaelhaydenduran",
+    },
+    {
+        gameURL: "https://pglenn1.github.io/charlie-project-alex/",
+        gameName: "Free Shrek!", 
+        gameAuthor: "Alex Glenn", 
+        authorGithubName: "pglenn1",
+    },
+    {
+        gameURL: "https://hkevin555.github.io/lab-charlie/",
+        gameName: "Help doge reach his goal!", 
+        gameAuthor: "Kevin Hernandez", 
+        authorGithubName: "hkevin555",
+    },
+    {
+        gameURL: "https://nesiagoodloe.github.io/nesia-charlie-lab/",
+        gameName: "Pick your favorite thing!", 
+        gameAuthor: "Rodnesia Goodloe", 
+        authorGithubName: "nesiagoodloe",
+    },
+    {
+        gameURL: "https://cm1803.github.io/charlie/",
+        gameName: "Can you make it to the end of the page?", 
+        gameAuthor: "Claire Mitchell", 
+        authorGithubName: "cm1803",
+    },
+    {
+        gameURL: "https://salehmb89.github.io/delta-lab/",
+        gameName: "", 
+        gameAuthor: "Saleh Basalim", 
+        authorGithubName: "salehmb89",
+    },
+    {
+        gameURL: "https://isabellerushing.github.io/crispy-octo-carnival-delta/",
+        gameName: "Get several facts about your name!", 
+        gameAuthor: "Isabelle Rushing", 
+        authorGithubName: "isabellerushing",
+    },
+    {
+        gameURL: "https://mmandelyn.github.io/silver-seal-charlie/",
+        gameName: "Hop to the finish line!", 
+        gameAuthor: "Mandy McGinnis", 
+        authorGithubName: "mmandelyn",
+    },
+    {
+        gameURL: "https://skysaunders.github.io/animated-sniffle-foxtrot/",
+        gameName: "Learn a bit about skyler!", 
+        gameAuthor: "Skyler Saunders", 
+        authorGithubName: "skysaunders",
+    },
+    {
+        gameURL: "https://cbuf0rd.github.io/glowing-meme-foxtrot/index.html",
+        gameName: "Learn a bit about Candice!", 
+        gameAuthor: "Candice Buford", 
+        authorGithubName: "cbuf0rd",
+    },
+    {
+        gameURL: "https://mohammedalrasheidi.github.io/Charlie/",
+        gameName: "Fun with doge!", 
+        gameAuthor: "Mohammed Alrasheidi", 
+        authorGithubName: "mohammedalrasheidi",
+    },
+    {
+        gameURL: "https://hrantgartley.github.io/didactic-waffle-delta/",
+        gameName: "Get lots of facts about your name!", 
+        gameAuthor: "Grant Hartley", 
+        authorGithubName: "hrantgartley",
+    },
+    {
+        gameURL: "https://aalluhaybi1.github.io/CHARLIE_LAB/",
+        gameName: "Charlie WOW 2.0", 
+        gameAuthor: "Ayman Alluhaybi", 
+        authorGithubName: "aalluhaybi1",
+    },
+    {
+        gameURL: "https://quantumcoder-242.github.io/turbo-delta/",
+        gameName: "Get a LOT of facts about your name!", 
+        gameAuthor: "Navraj Singh", 
+        authorGithubName: "quantumcoder-242",
+    },
+    {
+        gameURL: "https://akashdeepsingh0047.github.io/Chrlie-lab-By-Akashdeep/",
+        gameName: "Magic!", 
+        gameAuthor: "Akashdeep Singh", 
+        authorGithubName: "akashdeepsingh0047",
+    },
+
+
+];
